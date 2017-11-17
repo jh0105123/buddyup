f0ee5c15f762cf8804fbae9fba96878389a49035
kim <kim@kim-PC>
2017-11-17 11:10:10 +0900

    test

--- buddyup/src/buddyup/test.java
+++ buddyup/src/buddyup/test.java
@@ -3,6 +3,10 @@ package buddyup;
 public class test {
 public static void main(String[] args) {
 	String s = "방배 브런치에서 수정, 다른 파일 지움";
+	for (int j = 0; j < 3; j++) {
+		s.charAt(j);
+	}
+	
 }
 	int a = 12;
 
